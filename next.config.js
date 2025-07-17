290c244cd04b5f06f8495906f2ccf9e62b1ee47c
pritam modak <141545535+2006pritam@users.noreply.github.com>
2025-07-17 18:30:02 +0530

    Fix project save and deployment issues

--- next.config.js
+++ next.config.js
@@ -8,7 +8,21 @@ const nextConfig = {
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   output: 'export',
-  distDir: 'out'
+  distDir: 'out',
+  experimental: {
+    esmExternals: false
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
